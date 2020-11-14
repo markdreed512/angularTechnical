5213ba4ce133218956ac6267256afba9035602b6
Mark Reed <markdreed512@gmail.com>
2020-11-13 20:36:53 -0600

    contact reactive form

--- src/app/layout/contact/contact.component.ts
+++ src/app/layout/contact/contact.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent { }
+export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder){ }
+
+  ngOnInit(){
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.contactForm = this.fb.group({
+      name: "name",
+      email: "email",
+      message: "message",
+      shouldAddToNewsletter: false
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.contactForm)
+  }
+}
+
+
