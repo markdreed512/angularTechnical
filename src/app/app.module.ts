c04444489725fcbdad48bcd548aab0c05f1a9bf0
Mark Reed <markdreed512@gmail.com>
2020-11-18 15:13:45 -0600

    bug with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// import { MatInputModule } from '@angular/material/input';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatChipsModule } from '@angular/material/chips';
+
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -20,10 +28,16 @@ import { HeaderComponent } from './shared';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
     HttpModule,
+    // MatInputModule,
+    // MatSelectModule,
+    // MatButtonModule,
+    // MatCheckboxModule,
+    // MatChipsModule,
     NgbModule.forRoot()
   ],
   bootstrap: [AppComponent]
--- src/app/layout/contact/contact.component.ts
+++ src/app/layout/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
@@ -6,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent{
-
- log(x) {console.log(x);}
-
+  form = new FormGroup({
+      name:new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', Validators.required),
+      shouldAddToNewsletter: new FormControl()
+  })
 }
+  // onSubmit(){
+  //   console.log(this.form)
+  // }
+  // form = new FormGroup({
+  //   name: new FormControl('', Validators.required),
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   message: new FormControl('', Validators.required),
+  //   shouldAddToNewsletter: new FormControl()
+  // });
+
+//   get name(){
+//     return this.form.get('name');
+//   }
+//   get email(){
+//     return this.form.get('email')
+//   }
+//   get message(){
+//     return this.form.get('message')
+//   }
+
+//   log(x) {console.log(x);}
+//   submit(f) {
+//     console.log(f);
+//   }
+// }
