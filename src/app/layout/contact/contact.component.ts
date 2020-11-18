7934a2789e0a6799bbdab05da65839817211613f
Mark Reed <markdreed512@gmail.com>
2020-11-18 17:12:40 -0600

    success message

--- src/app/layout/contact/contact.component.ts
+++ src/app/layout/contact/contact.component.ts
@@ -7,12 +7,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent{
+  success: boolean = false;
   form = new FormGroup({
       name:new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       message: new FormControl('', Validators.required),
       shouldAddToNewsletter: new FormControl()
   })
+
+  onSubmit(){
+    this.success = true;
+    this.form.reset()
+  }
 }
   // onsubmit(){
   //   console.log(this.form)
